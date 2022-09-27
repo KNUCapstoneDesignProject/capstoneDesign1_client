55053b7baae42dbdbc7f810f25ddbe497e5beb0c
ehtjsv2 <ehtjsv2@naver.com>
2022-09-27 22:10:30 +0900

    first commit

---
+++ app/src/main/java/com/example/petsitter/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.petsitter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.Spinner
+import android.widget.TextView
+import org.mindrot.jbcrypt.BCrypt
+import org.w3c.dom.Text
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val input_id:TextView= findViewById(R.id.input_Id)
+        val input_passward:TextView = findViewById(R.id.input_Passward)
+        val login_btn: Button =findViewById(R.id.login_Btn)
+        val signIn_btn: TextView =findViewById(R.id.signup_Btn)
+        // Spinner
+        val animalSizeSpinner = findViewById<Spinner>(R.id.signup_Aniaml_Size)
+        //animalSizeSpinner.adapter=ArrayAdapter.createFromResource(this,R.array.Aniaml_Size,android.R.layout.simple_spinner_item)
+       // val addAnimal =findViewById<>(R.id.Animal02)
+
+        signIn_btn.setOnClickListener{
+            setContentView(R.layout.activity_signup)
+        }
+
+
+//        login_btn.setOnClickListener {
+//
+//            //비밀번호 암호화
+//            val passwordHashed = BCrypt.hashpw(input_passward.toString(), BCrypt.gensalt())
+//            //비밀번호 맞는지 확인.
+//            val isValidPassword = BCrypt.checkpw(input_passward.toString(), passwordHashed)
+//            println(passwordHashed)
+//            println(isValidPassword)
+//        }
+    }
+}
